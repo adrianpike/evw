5d74702a3048853ecd035be1374963003cc39e59
adrianpike <adrian.pike@gmail.com>
2010-10-04 22:49:03 -0700

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Evw::Application.config.secret_token = '5a41f5638d1b22648ffeb4416d8e2a74a7ca3fdaec604c1282a9bf70164bc27d460ab34460164037af0d0537c9b57debc4a0dee3b6f81409fd06c77881bbd169'
